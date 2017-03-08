84fcda5fba95405d348b22997c137b2788d73975
Jonathan Mares <jm2242@cornell.edu>
2017-03-07 22:22:34 -0500

    material-ui installed, but trying mui for grid system

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,16 +1,19 @@
 //@flow
 import React from 'react';
 import { Link } from 'react-router';
-
+import RaisedButton from 'material-ui/RaisedButton';
+import NavBar from './NavBar'
 const Main = React.createClass({
 
   render() {
     // Then we go ahead and return some JSX
     return (
       <div>
+        <NavBar />
         <h1>
           <Link to="/">Atacama</Link>
         </h1>
+        <RaisedButton label="Default" />
         {/* Do we need this?  */}
         {/* We use cloneElement here so we can auto pass down props */}
         {/* { React.cloneElement(this.props.children, this.props) } */}
--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute } from 'react-router'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import 'babel-polyfill'
+
 
 /*
   Import Components
@@ -21,18 +24,23 @@ import App from './components/App';
 /* Import our data store */
 import store, { history } from './store';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 /*
   Rendering
   This is where we hook up the Store with our actual component and the router
 */
 ReactDOM.render(
+  <MuiThemeProvider>
   <Provider store={store}>
     { /* Tell the Router to use our enhanced history */ }
     <Router history={history}>
       <Route path="/" component={App}>
       </Route>
     </Router>
-  </Provider>,
+  </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
