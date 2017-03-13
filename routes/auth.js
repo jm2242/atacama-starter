37deb8c38e271acde7c10b91d8f8e6b28d6763be
Lloyd Ramey <lnr0626@gmail.com>
2017-03-13 19:40:21 -0400

    switch back to profile/email

--- routes/auth.js
+++ routes/auth.js
@@ -57,8 +57,7 @@ passport.deserializeUser(function (id, done) {
 const route = express.Router();
 
 route.get('/google', passport.authenticate('google', {
-    scope: ['https://www.googleapis.com/auth/plus.login',
-        'https://www.googleapis.com/auth/plus.profile.emails.read']
+    scope: ['profile', 'email']
 }));
 
 route.get('/google/callback', passport.authenticate('google', {
