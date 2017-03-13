c5a84d86e81574f4296431ab704ea28ee2e34e45
Lloyd Ramey <lnr0626@gmail.com>
2017-03-13 19:36:28 -0400

    Add CRUD for database

--- database/book-lists.js
+++ database/book-lists.js
@@ -4,11 +4,23 @@ import connection from './mysql'
 import {Book, Tag, Author, BookList, User} from '../model'
 import {NotFound, Forbidden} from "../errors";
 import books from './books'
+import mysql from 'promise-mysql'
 
-function addBooksToBookList(conn, list: BookList, user: User) {
+function addBooksToBookList(conn: mysql.Connection, list: BookList, user: User) {
     return books.findAllInBookListWithConnection(conn, list.id, user).then((books: Book[]) => list.withBooks(books))
 }
 
+function _findOne(conn: mysql.Connection, id: number, user: User) {
+    return conn.query('SELECT * FROM BookList WHERE BookList.id = ? AND BookList.owner = ?', [id, user.id])
+        .then((res: any[]) => {
+            if (res.length != 1) {
+                throw new NotFound(`BookList id ${id} not found`);
+            }
+
+            return new BookList(res[0]);
+        })
+        .then((list: BookList) => addBooksToBookList(conn, list, user));
+}
 export default {
     findAll(offset: number, count: number, user: User): Bluebird {
         if (!user) {
@@ -28,16 +40,66 @@ export default {
             throw new Forbidden("You must be logged in to view this page");
         }
         return Bluebird
-            .using(connection(), conn =>
-                conn.query('SELECT * FROM BookList WHERE BookList.id = ? AND BookList.owner = ?', [id, user.id])
-                    .then((res: any[]) => {
-                        if (res.length != 1) {
-                            throw new NotFound(`BookList id ${id} not found`);
-                        }
+            .using(connection(), conn => _findOne(conn, id, user));
+    },
 
-                        return new BookList(res[0]);
-                    })
-                    .then((list: BookList) => addBooksToBookList(conn, list, user))
-            );
+    create(data: any, user: User): Bluebird {
+        if (!user) {
+            throw new Forbidden("You must be logged in to view this page");
+        }
+
+        const params = {
+            name: data.name,
+            description: data.description,
+            owner: user.id
+        };
+
+        return Bluebird
+            .using(connection(), conn => {
+                return conn.query('INSERT INTO BookList SET ?', params)
+                    .then((res: any) => conn.query('SELECT * FROM BookList WHERE id = ?', res.insertId))
+                    .then((results: any[]) => new BookList(results[0]))
+                    .then(list => addBooksToBookList(conn, list, user));
+            });
+    },
+
+    update(id: number, data: any, user: User) {
+        if (!user) {
+            throw new Forbidden("You must be logged in to view this page");
+        }
+
+        const params = {};
+
+        if (data.name) {
+            Object.extend(params, {name: data.name});
+        }
+
+        if (data.description) {
+            Object.extend(params, {description: data.description});
+        }
+
+        return Bluebird.using(connection(), conn =>
+            conn.query('UPDATE BookList SET ? WHERE id = ?', [params, id])
+                .then(() => _findOne(conn, id, user))
+        );
+    },
+
+    delete(id: number, user: User) {
+        if (!user) {
+            throw new Forbidden("You must be logged in to view this page");
+        }
+
+        const params = [id, user.id];
+
+        return Bluebird.using(connection(), conn =>
+            conn.query('SELECT * FROM BookList WHERE id = ? AND owner = ?', params)
+                .then((results: any[]) => {
+                    if (results.length != 1) {
+                        throw new Forbidden(`You cannot delete BookList with ID ${id}`);
+                    }
+                })
+                .then(() => conn.query('DELETE FROM BookList WHERE id = ? AND owner = ?', params))
+                .then(() => conn.query('DELETE FROM Contains WHERE book_list_id = ?', [id]))
+        );
     }
 }
