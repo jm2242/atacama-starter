6bf23ab9573d0b448a065a0e5c7b519b2c14daa6
Jonathan Mares <jm2242@cornell.edu>
2017-03-29 11:28:55 -0400

    install packages

--- client/src/containers/Reader.js
+++ client/src/containers/Reader.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import {
-  EpubView, // Underlaying epub-canvas (wrapper for epub.js iframe)
-  EpubViewStyle, // Styles for EpubView, you can pass it to the instance as a style prop for customize it
-  ReactReader, // A simple epub-reader with left/right button and chapter navigation
-  ReactReaderStyle // Styles for the epub-reader it you need to customize it
-} from 'react-reader'
-
+import { ReactReader } from 'react-reader'
+import 'babel-polyfill'
 
 class Reader extends Component {
   render() {
     return (
       <div style={{position: 'relative', height: '100%'}}>
         <ReactReader
-          url={'/api/books/12176/full-text'}
+          url={'https://s3-eu-west-1.amazonaws.com/react-reader/alice.epub'}
           title={'Alice in wonderland'}
           location={'epubcfi(/6/2[cover]!/6)'}
           locationChanged={(epubcifi) => console.log(epubcifi)}
