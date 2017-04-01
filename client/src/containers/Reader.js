dcd1e7002c170c97ba80b61f1e1e607e7d6ae1ce
Jonathan Mares <jm2242@cornell.edu>
2017-04-01 09:39:24 -0400

    wired up react reader with demo book

--- client/src/containers/Reader.js
+++ client/src/containers/Reader.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import { ReactReader } from 'react-reader'
 import 'babel-polyfill'
 
+
+// Styles
+import styles from '../styles/reader.css'
+
+
+const storage = global.localStorage || null
+
 class Reader extends Component {
+  constructor (props) {
+   super(props)
+   this.state = {
+     fullscreen: false,
+     location: (storage && storage.getItem('epub-location')) ? storage.getItem('epub-location') : 0
+   }
+ }
+
+ onLocationChanged = (loc) => {
+    storage && storage.setItem('epub-location', loc)
+  }
+
   render() {
+    const {fullscreen, location} = this.state
     return (
-      <div style={{position: 'relative', height: '100%'}}>
-        <ReactReader
-          url={'https://s3-eu-west-1.amazonaws.com/react-reader/alice.epub'}
-          title={'Alice in wonderland'}
-          location={'epubcfi(/6/2[cover]!/6)'}
-          locationChanged={(epubcifi) => console.log(epubcifi)}
-        />
+      <div className="container">
+        <div className="readerHolder">
+          <ReactReader
+            url={'https://s3-eu-west-1.amazonaws.com/react-reader/alice.epub'}
+            title={'Alice in wonderland'}
+            location={location}
+            locationChanged={this.onLocationChanged}
+          />
+        </div>
       </div>
     )
   }
